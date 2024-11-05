c4ec66b01f4b70f10d7528a43f8b652187ca849d
Evan junior <evsioljr@gmail.com>
2024-11-05 20:43:34 -0300

    criaçãon de projeto

---
+++ taugor/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='container'>
+        <div className='container2'>
+          <div className='fale'></div>
+          <div className='infbase'></div>
+          <div className='empreende'></div>
+          <div className='infor'></div>
+        </div>
+
+        <div className='buttonsnavigate'>
+          
+        </div>
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
